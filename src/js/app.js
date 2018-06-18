cc9138fcb4cabed02d1c79003af4a52b57ba7ea0
Sen4az <simon.z@yandex.ru>
2018-06-18 03:52:20 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,3 @@ require('../css/app.css');
 console.log('Paste your code here!');
 console.log(testModules.hello);
 
-
-const items = document.querySelectorAll('.card');
-const itemsArray = Array.prototype.slice.call(items);
-
-itemsArray.map(function(item) {
-	item.addEventListener('click', toggleClass);
-});
-
-function toggleClass() {
-	itemsArray.map(function(item) {
-		item.classList.remove('visible');
-	});
-	this.classList.toggle('visible');
-}
